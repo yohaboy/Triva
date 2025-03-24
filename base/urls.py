b9c2ccd090198e74ccacc7ad5f1e24e1ece1ef8f
yohaboy <yohannesgizaw777@gmail.com>
2025-03-25 02:16:16 +0300

    front-first

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.home ,name='home')
+    path('',views.home ,name='home'),
+    path('rank/' ,views.rank,name='rank'),
+    path('profile/' ,views.profile,name='profile'),
 ]
--- base/views.py
+++ base/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request , "base/index.html")
+
+def rank(request):
+    return render(request , "base/rank.html")
+
+def profile(request):
+    return render(request , 'base/profile.html')
